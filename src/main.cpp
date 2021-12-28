15ed87dd2a0c46904e44d05956a238f704712c18
Gnomeball <gnome.c@protonmail.com>
2021-12-28 13:09:52 +0000

    Now read file input

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
+#include <cstdlib>
 #include <string>
+#include <iostream>
 
 #include "parser.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    std::string string = "../images/Duck.png";
-    Parser p = Parser(string);
+    std::string file_name;
+
+    if (argc == 1) {
+        std::cout << "no file name given" << std::endl;
+        exit(EXIT_FAILURE);
+    } else {
+        file_name = argv[1];
+        std::cout << "file : " << file_name << std::endl;
+        std::ifstream infile(file_name);
+        if (!infile.good()) {
+            std::cout << "file cannot be read" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    Parser p = Parser(file_name);
 
     p.write_grid_to_file();
 
